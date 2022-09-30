e68c20314627d316ca8c65d893106da468e87e5e
Victor Hernandez <v.hernandez@cgiar.org>
2022-09-30 09:44:50 -0500

    agregada informacions de Clusters a la api

--- src/edaphoclimatic_api/api.py
+++ src/edaphoclimatic_api/api.py
@@ -1,3 +1,4 @@
+from unicodedata import numeric
 from flask import Flask, request, jsonify, make_response, render_template
 from flask_cors import cross_origin
 import numpy as np
@@ -6,6 +7,7 @@ from os.path import abspath, dirname, join
 from pyproj import Transformer
 import rasterio
 import json
+import pandas as pd
 
 # Define the application directory
 BASE_DIR = dirname(abspath(__file__))
@@ -42,17 +44,43 @@ def get_data():
             files_tif = [f for f  in files if f[-3:] == 'tif']
             c={}
             for f in files_tif:
-                with rasterio.open(join(path, f)) as raster:
-                    var = f.replace('_rfcovar.tif', '')
-                    transformer = Transformer.from_crs("EPSG:4326", raster.crs, always_xy=True)
-                    xx, yy = transformer.transform(longitude, latitude)
-                    value = list(raster.sample([(xx, yy)]))[0][0]
-                    c[var] = round(np.float64(value), 5)
-            normal = False
+                if '_rfcovar.tif' in f :
+                    with rasterio.open(join(path, f)) as raster:
+                        var = f.replace('_rfcovar.tif', '')
+                        transformer = Transformer.from_crs("EPSG:4326", raster.crs, always_xy=True)
+                        xx, yy = transformer.transform(longitude, latitude)
+                        value = list(raster.sample([(xx, yy)]))[0][0]
+                        c[var] = round(np.float64(value), 5)
+            normal = True
 
             for dato in c.values():
-                if dato != -3.3999999521443642e+38:
-                    normal = True
+                if dato == -3.3999999521443642e+38:
+                    normal = False
+
+            f = "clusters.tif"
+            numClus= 0
+            c['cluster'] = {}
+
+            with rasterio.open(join(path, f)) as raster:
+                transformer = Transformer.from_crs("EPSG:4326", raster.crs, always_xy=True)
+                xx, yy = transformer.transform(longitude, latitude)
+                value = list(raster.sample([(xx, yy)]))[0][0]
+                numClus = int(np.float64(value))
+
+            
+            
+            if numClus == -3.3999999521443642e+38:
+                c['cluster'] = {'message': "No hay zona Edafoclimática para estas coordenadas"}
+            else:
+                df = pd.read_excel(path+ "\data_clusters.xlsx", usecols=["variable", "rango", f'C{numClus}'])
+                df.fillna(0, inplace=True)
+                c['cluster']['numCluster'] = numClus
+                for index, row in df.iterrows():
+                    if not row['variable'] in c['cluster'] :
+                        c['cluster'][row['variable']] = {}
+                    c['cluster'][row['variable']][row['rango']] = row[f'C{numClus}']
+
+
             if normal:
                 c["Ca_Mg"] = round(c["Ca"] / c["Mg"], 5)
                 c["Ca_K"] = round(c["Ca"] / c["K"], 5)
@@ -88,6 +116,65 @@ def get_info():
         data = json.load(file)
     return data
 
+
+@app.route('/api/v1/cluster', methods=['GET'])
+@cross_origin()
+def get_cluster():
+
+    try:
+        longitude = request.args.get("longitude")
+        latitude = request.args.get("latitude")
+        if longitude and latitude :
+
+            if int(float(latitude)) > 90 or int(float(latitude)) < -90:
+                return jsonify({
+                    'message': "Latitud por fuera de los valores normales"
+                })
+
+            if int(float(longitude)) > 180 or int(float(longitude)) < -180:
+                return jsonify({
+                    'message': "Longitud por fuera de los valores normales"
+                })
+            path = join(BASE_DIR, 'data')
+            f = "clusters.tif"
+            c= 0
+            with rasterio.open(join(path, f)) as raster:
+                transformer = Transformer.from_crs("EPSG:4326", raster.crs, always_xy=True)
+                xx, yy = transformer.transform(longitude, latitude)
+                value = list(raster.sample([(xx, yy)]))[0][0]
+                c = int(np.float64(value))
+
+            normal = True
+            
+            if c == -3.3999999521443642e+38:
+                normal = False
+            if normal:
+                df = pd.read_excel(path+ "\data_clusters.xlsx", usecols=["variable", "rango", f'C{c}'])
+                df.fillna(0, inplace=True)
+                cluster = {}
+                cluster['numCluster'] = c
+                for index, row in df.iterrows():
+                    if not row['variable'] in cluster :
+                        cluster[row['variable']] = {}
+                    cluster[row['variable']][row['rango']] = row[f'C{c}'] 
+
+                print('Para las coordenadas:', 'Latitud: '+ latitude, 'Longitud: ' + longitude, 'los datos del cluster son: ', sep='\n' )
+                print(cluster)
+                return jsonify(cluster)
+            else:
+                print('las coordenadas:', 'Latitud: '+ latitude, 'Longitud: ' + longitude, 'estan fuera de los limites', sep='\n' )
+                return jsonify({
+                    'message': "No hay zona Edafoclimática para estas coordenadas"
+                })
+        else:
+            return jsonify({
+                'message': "No hay Longitud o Latitud"
+            })
+    except Exception as e:
+        print("type error: " + str(e))
+        return jsonify({
+                'message': "Error en el servidor"
+            })
 
 
 if __name__ == '__main__':
